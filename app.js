ec209ca95af38aff8a4ce8995fc5d54149300694
Naga Satya Geeth <67518197+Artistole@users.noreply.github.com>
2021-08-21 20:51:00 +0530

    refactor

---
+++ app.js
@@ -0,0 +1,42 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outPutBox = document.querySelector("#output-box");
+
+function compareValues(sum,luckyNumber)
+{
+  if(sum%luckyNumber===0)
+  {
+      outPutBox.innerText="Your birthday is lucky";
+  }
+  else
+  {
+     outPutBox.innerText="Your birthday is not lucky";
+  }
+}
+
+
+function checkBirthDateIsLucky()
+{
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if(sum&&dob)
+    compareValues(sum, luckyNumber.value)
+    else
+    outPutBox.innerText="Please enter both the fields "
+}
+
+function calculateSum(dob) 
+{
+  dob = dob.replaceAll("-"," ");
+  let sum =0;
+  for(let index=0;index<dob.length;index++)
+  {
+      sum = sum + Number(dob.charAt(index));
+
+  }
+    return sum;
+}
+
+
+checkNumberButton.addEventListener('click', checkBirthDateIsLucky)
